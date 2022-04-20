d0da8d5bb79c87eae8145ec2b8116f58a2c839e5
xavierpuigf <xavierpuig@csail.mit.edu>
2022-04-20 13:33:04 -0400

    configs for helping

--- helping_states_latest.sh
+++ helping_states_latest.sh
@@ -13,12 +13,29 @@ CUDA_VISIBLE_DEVICES=1 python analysis/helping_states_diffpred.py \
 # KL 0.0
 export rootpath="/data/vision/torralba/frames/data_acquisition/SyntheticStories/online_wah/ckpts/predict_graph/train_data.dataset_graph_full_150step_larger_train.pkl/lr0.0009-bs.256-klcoff.0.0-goalenc.False_predchange.none_inputgoal.False_excledge.True_preddiff.Falsereduced_walk_logname.newvaefull_encoder_task_graph_condprior.False_zvec.node"
 export ck=""
+export full_ck=$rootpath$ck
+
+CUDA_VISIBLE_DEVICES=1 python analysis/helping_states_diffpred.py \
+    agent_pred_graph="config_vae0.000" num_processes=20 \
+    agent_pred_graph.ckpt_load=$full_ck"/290.pt" base_port=8183 hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=disabled hydra/hydra_logging=disabled
 
 
 # KL 1.0
 export rootpath="/data/vision/torralba/frames/data_acquisition/SyntheticStories/online_wah/ckpts/predict_graph/train_data.dataset_graph_full_150step_larger_train.pkl/lr0.0009-bs.256-klcoff.1-goalenc.False_predchange.none_inputgoal.False_excledge.True_preddiff.Falsereduced_walk_logname.newvaefull_encoder_task_graph_condprior.False_zvec.node"
 export ck=""
+export full_ck=$rootpath$ck
+
+CUDA_VISIBLE_DEVICES=1 python analysis/helping_states_diffpred.py \
+    agent_pred_graph="config_vae1.000" num_processes=20 \
+    agent_pred_graph.ckpt_load=$full_ck"/290.pt" base_port=8183 hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=disabled hydra/hydra_logging=disabled
 
 
 # Deterministic
 export rootpath="/data/vision/torralba/frames/data_acquisition/SyntheticStories/online_wah/ckpts/predict_graph/train_data.dataset_graph_full_150step_larger_train.pkl/lr0.0009-bs.256-klcoff.1-goalenc.False_predchange.none_inputgoal.False_excledge.True_preddiff.Falsereduced_walk_logname.detfull_encoder_task_graph"
+
+export ck=""
+export full_ck=$rootpath$ck
+
+CUDA_VISIBLE_DEVICES=1 python analysis/helping_states_diffpred.py \
+    agent_pred_graph="config_det" num_processes=20 \
+    agent_pred_graph.ckpt_load=$full_ck"/290.pt" base_port=8183 hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=disabled hydra/hydra_logging=disabled
